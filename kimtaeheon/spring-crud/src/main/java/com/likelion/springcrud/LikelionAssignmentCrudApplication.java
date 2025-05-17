abe8bc99191d524195c471080ed266a9b40464c5
KTH1007 <taeheon@gimtaeheon-ui-MacBookPro.local>
2025-05-17 22:53:16 +0900

    Rename: 프로젝트 이름 변경

--- kimtaeheon/spring-crud/src/main/java/com/likelion/springcrud/LikelionAssignmentCrudApplication.java
+++ kimtaeheon/spring-crud/src/main/java/com/likelion/springcrud/LikelionAssignmentCrudApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCrudApplication {
+public class LikelionAssignmentCrudApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCrudApplication.class, args);
+        SpringApplication.run(LikelionAssignmentCrudApplication.class, args);
     }
 
 }
